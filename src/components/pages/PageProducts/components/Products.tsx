40f0dec61c7f8f4b0faee94ab1718060695ead9d
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-09-18 14:44:03 +0300

    complite fe cloudFront - http://d3ol1hsavksg0r.cloudfront.net s3 - http://my-fe-store-app.s3-website-eu-west-1.amazonaws.com/

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -69,28 +69,3 @@ export default function Products() {
     </Grid>
   );
 }
-
-//  return (
-//    <Grid container spacing={4}>
-//      {products.map((product: Product, index: number) => (
-//        <Grid item key={product.id} xs={12} sm={6} md={4}>
-//          <Card className={classes.card}>
-//            <CardMedia
-//              className={classes.cardMedia}
-//              image={product.image}
-//              title={product.title}
-//            />
-//            <CardContent className={classes.cardContent}>
-//              <Typography gutterBottom variant="h5" component="h2">
-//                {product.title}
-//              </Typography>
-//              <Typography>{formatAsPrice(product.price)}</Typography>
-//            </CardContent>
-//            <CardActions>
-//              <AddProductToCart product={product} />
-//            </CardActions>
-//          </Card>
-//        </Grid>
-//      ))}
-//    </Grid>
-//  );
